a0f71b9c0ce604e13860348b5be927d9f7b33f00
Badgujar Dipesh <117906055+dipeshb1999@users.noreply.github.com>
2023-05-31 17:01:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
-function threeSum(arr, target) {
-// write your code here
-  
+function convertToBinary() {
+  const decimalNumber = document.getElementById("decimalNumber").value;
+  const binaryNumber = decimalToBinary(decimalNumber);
+  document.getElementById("result").textContent =
+    "Binary equivalent: " + binaryNumber;
 }
 
-module.exports = threeSum;
+function decimalToBinary(decimalNumber) {
+  let binaryNumber = "";
+
+  while (decimalNumber > 0) {
+    binaryNumber = (decimalNumber % 2) + binaryNumber;
+    decimalNumber = Math.floor(decimalNumber / 2);
+  }
+
+  return binaryNumber;
+}
