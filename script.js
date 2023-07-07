0e0003e35c9e60cd03cffc5a3c82c8f38fe483b4
Umakant Dwivedi <78226651+ukdgit@users.noreply.github.com>
2023-07-07 16:08:07 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,5 @@
-function convertToBinary() {
-  const decimalNumber = document.getElementById("decimalNumber").value;
-  const binaryNumber = decimalToBinary(decimalNumber);
-  document.getElementById("result").textContent =
-    "Binary equivalent: " + binaryNumber;
+function decimalToBinary(num) {
+  // Write your code here
 }
 
-function decimalToBinary(decimalNumber) {
-  let binaryNumber = "";
-
-  while (decimalNumber > 0) {
-    binaryNumber = (decimalNumber % 2) + binaryNumber;
-    decimalNumber = Math.floor(decimalNumber / 2);
-  }
-
-  return binaryNumber;
-}
+window.decimalToBinary = decimalToBinary;
